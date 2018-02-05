bcddab90835d3607a1314b5e98bbc38cd689bed0
Ayan <ayan.kurmanbai@gmail.com>
2018-02-05 22:22:32 +0600

    added queue implementation

---
+++ GameOfLife/GameOfLife/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{ 
+    class Program
+    {
+        const int n = 20;
+
+        static char[,] primary = new char[n + 1, n + 1];
+        static char[,] secondary = new char[n + 1, n + 1];
+        static bool[,] used = new bool[n + 1, n + 1];
+
+        static int[] dx = new int[] {0, 0, -1, 1, -1, -1, 1, 1};
+        static int[] dy = new int[] {-1, 1, 0, 0, -1, 1, -1, 1};
+
+        static bool check(int x, int y)
+        {
+            if (x >= 1 && x <= n && y >= 1 && y <= n)
+                return true;
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You are given a field with a not alive cells");
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write('.' + " ");
+                    primary[i, j] = '.';
+                    secondary[i, j] = '.';
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("Enter number queries");
+            int queries = Convert.ToInt32(Console.ReadLine());
+            while(queries != 0)
+            {
+                Console.Write("Please enter x y: ");
+                string input = Console.ReadLine();
+                string[] splitted = input.Split(' ');
+                int x = Convert.ToInt32(splitted[0]);
+                int y = Convert.ToInt32(splitted[1]);
+                if (check(x, y))
+                {
+                    if (!used[x, y])
+                    {
+                        primary[x, y] = 'o';
+                        used[x, y] = true;
+                        queries--;
+                        print();
+                    }
+                    else
+                    {
+                        Console.WriteLine("This coordinates have already entered");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("x or y value exceeds restricted range from 1 to {0}", n);
+                }                
+            }
+
+            Console.Clear();
+            print();
+            while(true)  
+            {
+                Thread fill = new Thread(thread1);
+                fill.Start();
+                Thread delete = new Thread(thread2);
+                delete.Start();
+                Thread.Sleep(600);
+
+                fillPoints();
+                assign();
+            }
+
+            Console.ReadKey();
+        }
+                                                                                                                                                                                                 
+        static void fillPoints()
+        {
+            for (int i = 1; i <= n; i++)
+            { 
+                for (int j = 1; j <= n; j++)
+                {
+                    if (primary[i, j] != secondary[i, j] || (primary[i, j] == secondary[i, j] && secondary[i, j] == 'o'))
+                    {
+                        Console.SetCursorPosition((j - 1) * 2, i - 1);
+                        Console.Write(secondary[i, j]);   
+                    }
+                }
+            }
+        }
+
+        static void thread1()
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if (primary[i, j] == '.')
+                    {
+                        checkCell(i, j, false);
+                    }                                                                                                                                 
+                }
+            }
+        }
+
+        static void thread2()
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if (primary[i, j] == 'o')
+                    {
+                        checkCell(i, j, true);
+                    }
+                }
+            }
+        }
+
+        static void assign()
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    primary[i, j] = secondary[i, j];
+                    secondary[i, j] = '.';
+                }
+            }
+        }
+        
+        static void checkCell(int x, int y, bool alive) 
+        {
+            int cnt = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                int qx = x + dx[i], qy = y + dy[i];
+                if (check(qx, qy) && primary[qx, qy] == 'o') 
+                {
+                    cnt++;
+                }
+            }
+            secondary[x, y] = primary[x, y];
+            if (alive && (cnt <= 1 || cnt >= 4))
+            { 
+                secondary[x, y] = '.';
+            }
+            if (!alive && cnt == 3)
+            {
+                secondary[x, y] = 'o';
+            }
+        }
+
+        static void print()
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write(primary[i, j] + " ");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ Queue/Queue/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Program
+    {
+        static Queue<int> q = new Queue<int>();
+
+        static List<string> arrival = new List<string>();
+        static List<string> interArrival = new List<string>();
+        static List<string> service = new List<string>();
+
+        static DateTime startTime = DateTime.Now;
+        const int n = 10, numOfOpInQueue = 40, numberOfTrFiles = 100;
+        static int min = 0, sec = 0, mil = 0;
+        static bool state = false;
+
+        static void print()
+        {
+            Console.Write("P Num | ");
+            Console.Write("Arr Time | ");
+            Console.Write("IntArr Time | ");
+            Console.WriteLine("Ser Time");
+        }
+
+        static void Main(string[] args)
+        {
+            print();
+
+            for (int i = 1; i <= n; i++)
+            {
+                q.Enqueue(i * 10);
+                generator(i * 10);
+            }
+            Console.ReadKey();
+        }
+
+        static void generator(int query)
+        {
+            if (!state)
+            {
+                state = true;
+                Thread thread = new Thread(queue);
+                thread.Start();
+            }
+            arrival.Add(String.Format("{0}.{1}.{2}", min, sec, mil));
+
+            Thread.Sleep(Convert.ToInt32(query * 1000.0 / numberOfTrFiles));
+
+            TimeSpan time = DateTime.Now - startTime;
+            min = time.Minutes; sec = time.Seconds; mil = time.Milliseconds;
+            interArrival.Add(String.Format("{0}.{1}.{2}", min, sec, mil));
+        }
+
+        static void queue()
+        {
+            int i = 0;
+            while(q.Count != 0)
+            {
+                DateTime currentTime = DateTime.Now;
+                int query = q.Dequeue();
+                double delay = query * 1000.0 / numOfOpInQueue;
+                Thread.Sleep(Convert.ToInt32(delay));
+                TimeSpan time = DateTime.Now - currentTime;
+                service.Add(String.Format("{0}.{1}.{2}", time.Minutes, time.Seconds, time.Milliseconds));
+
+                if (i < n)
+                {
+                    Console.WriteLine("{0}       {1}       {2}       {3}",
+                                i + 1, arrival[i], interArrival[i], service[i]);
+                    i++;
+                }
+            }
+        }
+    }
+}
